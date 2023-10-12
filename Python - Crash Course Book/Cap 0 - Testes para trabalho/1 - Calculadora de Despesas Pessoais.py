8aee38b01c6cfc09955492487f3bf50a2990d138
Alan Tesseroli <118698111+AlanTesseroli@users.noreply.github.com>
2023-10-12 11:13:32 -0300

    Teste para trabalho

---
+++ Python - Crash Course Book/Cap 0 - Testes para trabalho/1 - Calculadora de Despesas Pessoais.py
@@ -0,0 +1,29 @@
+class Despesa():
+    def __init__(self, nome, valor):
+        self.nome = nome
+        self.valor = valor
+
+
+while True:
+    print('CALCULADORA DE DESPESAS')
+    print('Opções: ')
+    print('1. Adicionar despesa')
+    print('2. Listas despesas')
+    print('3. Calcula soma de despesas')
+    print('4. Sair')
+
+
+    opcao = int(input('Selecione: '))
+
+    if opcao = 1:
+    
+    elif opcao = 2:
+
+    elif opcao = 3:
+
+    elif opcao = 4:
+        print('Certo, até mais!')
+        break
+
+    else:
+        print('Tente novamente, o valor não foi aceito')
